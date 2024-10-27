c48adfb52f6cefe0905596782af63bf43703de94
yuu <crosscore.g@gmail.com>
2024-10-27 13:16:38 +0900

    Add: a script for langchain

---
+++ 3/1_langchain.py
@@ -0,0 +1,25 @@
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage, SystemMessage
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+BEDROCK_MODEL_ID = os.getenv('BEDROCK_MODEL_ID')
+
+chat = ChatBedrock(
+    model_id=BEDROCK_MODEL_ID,
+    model_kwargs={"max_tokens": 128},
+    streaming=True
+)
+
+messages = [
+    SystemMessage(content="あなたは親切なAIアシスタントです"),
+    HumanMessage(content="Amazon Bedrockの名称の由来を100文字以内で教えて")
+]
+
+try:
+    for chunk in chat.stream(messages):
+        print(chunk.content, end="", flush=True)
+
+except Exception as e:
+    print(f"エラーが発生しました: {e}")
