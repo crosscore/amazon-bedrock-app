702bbe26605fbf55ca1b29c2cb128e12631dd213
yuu <crosscore.g@gmail.com>
2024-10-27 09:20:10 +0900

    Add: script for ai21_tokenizer

--- 3/1_ai21lab-token.py
+++ 3/1_ai21lab-token.py
@@ -1,1 +1,11 @@
 from ai21_tokenizer import Tokenizer
+
+tokenizer = Tokenizer.get_tokenizer()
+
+text = "Amazon BedrockはAWSの生成AIサービスです"
+
+encoded_text = tokenizer.encode(text)
+print(encoded_text)
+
+convert_tokens = tokenizer.convert_ids_to_tokens(encoded_text)
+print(convert_tokens)
